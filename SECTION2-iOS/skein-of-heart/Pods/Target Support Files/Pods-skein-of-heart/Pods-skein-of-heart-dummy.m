3812f5599d602def628defe83c4a9b8ed1ee31f6
dev-hjJoo <dodam.janna1997@gmail.com>
2020-05-20 17:03:05 +0900

    delete all files without Swift.gitignore

--- SECTION2-iOS/skein-of-heart/Pods/Target Support Files/Pods-skein-of-heart/Pods-skein-of-heart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_skein_of_heart : NSObject
-@end
-@implementation PodsDummy_Pods_skein_of_heart
-@end
--- SECTION2-iOS/skein-of-heart/skein-of-heart/Diary.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Diary.swift
-//  skein-of-heart
-//
-//  Created by hyovely on 2020/05/13.
-//  Copyright © 2020 MobileX.IVIS.CWNU. All rights reserved.
-//
-
-import Foundation
