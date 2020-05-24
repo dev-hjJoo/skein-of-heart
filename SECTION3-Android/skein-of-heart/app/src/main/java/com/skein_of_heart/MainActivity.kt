c3c0deb7b838e1faeca96540f038726af3d1726e
leesongmi <lssong9631@gmail.com>
2020-05-24 22:22:41 +0900

    google login

--- SECTION3-Android/skein-of-heart/app/src/main/java/com/skein_of_heart/MainActivity.kt
+++ SECTION3-Android/skein-of-heart/app/src/main/java/com/skein_of_heart/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.skein_of_heart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+    // 로그인 설정
+    private val RC_SIGN_IN = 9999
+    private var googleSigninClient: GoogleSignInClient? = null
+    private var firebaseAuth: FirebaseAuth? = null
+
+    var TAG = "test"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSigninClient = GoogleSignIn.getClient(this, gso)
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        var loginBtn: SignInButton = findViewById(R.id.googleLogin)
+
+        loginBtn.setOnClickListener {
+            val signInIntent = googleSigninClient!!.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
     }
+
+    //로그인 fun
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val user = firebaseAuth?.currentUser
+                } else {
+                }
+            }
+    }
+
 }
---
+++ SECTION3-Android/skein-of-heart/app/src/main/java/com/skein_of_heart/sign_in.kt
@@ -0,0 +1,8 @@
+package com.skein_of_heart
+import android.widget.Button
+
+
+class sign_in {
+
+
+}
