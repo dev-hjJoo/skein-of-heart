87671493bfb5fd08813c9f69e2e5c8d2af195244
LeeSongMi <lssong9631@gmail.com>
2020-11-03 19:22:31 +0900

    마이페이지

--- SECTION4-Front/src/index.js
+++ SECTION4-Front/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import MainContainer from "./js/screens/main/MainContainer";
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    {/* <App /> */}
+    <MainContainer />
+  </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ SECTION4-Front/src/js/components/SkeinOfHeart.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../screens/main";
+import DiaryList from "../screens/diaryList";
+import DiaryWrite from "../screens/diaryWrite";
+import Login from "../screens/login";
+import MyPage from "../screens/myPage";
+
+const SkeinOfHeart = () => {};
+export default SkeinOfHeart;
